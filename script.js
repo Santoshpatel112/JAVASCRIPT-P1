431be8469855f59a6f6db475a854b26797eae979
santosh patel <santoshpatelvns5@gmail.com>
2025-09-13 23:30:35 +0530

    index and script file setup

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 // Words vS Keywords
+// lolo -> words
 
 // Keyword -> on its meaning
-// let const var
+// let const var if , else function
 // jaise me iase word jinse kuchh ho skta hai vo js ke keyword hai 
